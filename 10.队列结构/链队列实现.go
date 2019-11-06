826f8d75c3f4bdf0e245088b67060334f2a300a9
百里 <freeit@126.com>
2019-11-06 10:58:03 +0800

    块链结构

---
+++ 10.队列结构/链队列实现.go
@@ -0,0 +1,12 @@
+package main
+
+type LinkedNode struct {
+	front *LinkedNode //前趋指针
+	rear  *LinkedNode //后续指针
+	data  interface{} //存储的值
+}
+
+type LinkedQueue struct {
+	Head *LinkedNode //头指针
+	Tail *LinkedNode //尾指针
+}
---
+++ 11.串结构/01.块链结构.go
@@ -0,0 +1,32 @@
+/*
+利用块链存储,提高存储密度
+存储密度公式=串值所占的存储/实际分配的存储
+*/
+package main
+
+import "unsafe"
+
+//定义一个块链结点
+type ChunkNode struct {
+	ch   []byte
+	next *ChunkNode
+}
+
+func NewChunkNode(data string) *ChunkNode {
+	c := &ChunkNode{
+		ch: make([]byte, 0),
+	}
+	c.ch = append(c.ch, string2byte(data)...)
+	return c
+}
+func byte2string(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+func string2byte(s string) []byte {
+	return *(*[]byte)(unsafe.Pointer(&s))
+}
+
+type ChunkLinked struct {
+	Head *ChunkNode
+	Tail *ChunkNode
+}
