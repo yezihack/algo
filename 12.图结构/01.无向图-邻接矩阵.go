93c83ec5284e7735f44aba641e6cb861ebf04bcb
百里 <freeit@126.com>
2019-11-11 09:50:13 +0800

    邻接表表示法

--- 12.图结构/01.无向图-邻接矩阵.go
+++ 12.图结构/01.无向图-邻接矩阵.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type VerTextType byte //顶点类型
 type ArcType int      //边的权值类型
 
@@ -7,6 +9,28 @@ type ArcType int      //边的权值类型
 type AMGraph struct {
 	VerTexType []VerTextType //顶点表
 	ArcType    [][]ArcType   //邻接矩阵
-	verNum     int           //图的当前点数
+	verNum     int           //图的顶点数
 	arcNum     int           //图的边数
+}
+
+func CreateUDN(verList []VerTextType) *AMGraph {
+	max := len(verList)
+	arcList := make([][]ArcType, max)
+	for i := 0; i < max; i++ {
+		arcCol := make([]ArcType, max)
+		for j := 0; j < max; j++ {
+			arcCol[j] = 1<<32 - 1
+		}
+		arcList[i] = arcCol
+	}
+	return &AMGraph{
+		VerTexType: verList,
+		ArcType:    arcList,
+		verNum:     max,
+	}
+}
+func main() {
+	ver := []VerTextType{'A', 'B', 'C', 'D'}
+	udn := CreateUDN(ver)
+	fmt.Println(udn.ArcType)
 }
