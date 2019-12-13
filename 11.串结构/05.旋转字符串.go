53fbac2a5a5f2dc08ff2368698773627f9d7649d
百里 <freeit@126.com>
2019-12-13 23:19:48 +0800

    algo

---
+++ 11.串结构/05.旋转字符串.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func LeftShiftOne(s string) string {
+	b := []byte(s)
+	one := b[0]
+	for i := 1;i < len(b); i ++ {
+		b[i-1] = b[i]
+	}
+	b[len(b)-1] = one
+	return string(b)
+}
+func LeftShiftTwo(s string) string {
+	b := []byte(s)
+	t1 := b[0]
+	t2 := b[1]
+	for i := 2;i < len(b);i ++ {
+		b[i-2] = b[i]
+	}
+	b[len(b)-1] = t2
+	b[len(b)-2] = t1
+	return string(b)
+}
+func main() {
+	//abcdef->cdefab
+	fmt.Println(LeftShiftOne(LeftShiftOne("abcdef")))
+	fmt.Println(LeftShiftTwo("abcdef"))
+}
